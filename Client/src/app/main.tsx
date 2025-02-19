779a5e116f92c66caeb12b1f8211c3fdaf6933cb
Edanriell <Edanriell@gmail.com>
2025-02-19 07:09:14 +0200

    feature(initial-config): configured app

---
+++ Client/src/app/main.tsx
@@ -0,0 +1,17 @@
+import "./styles/styles.less";
+
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+
+import { TanStackRouterProvider } from "@app/providers";
+
+const rootElement = document.getElementById("root")!;
+
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(
+		<StrictMode>
+			<TanStackRouterProvider />
+		</StrictMode>
+	);
+}
--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { URL } from "url";
+
+const resolvePath = (path: string) => new URL(path, import.meta.url).pathname;
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		TanStackRouterVite({
+			routesDirectory: "./src/app/routes",
+			generatedRouteTree: "./src/app/routeTree.gen.ts"
+		}),
+		react()
+	],
+	resolve: {
+		alias: {
+			"@app": resolvePath("./src/app"),
+			"@entities": resolvePath("./src/entities"),
+			"@features": resolvePath("./src/features"),
+			"@pages": resolvePath("./src/pages"),
+			"@shared": resolvePath("./src/shared"),
+			"@widgets": resolvePath("./src/widgets")
+		}
+	}
+});
