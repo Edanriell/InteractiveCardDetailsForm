805f5aebda5982f83ac0ecd7a10659ee51f85dd7
Edanriell <Edanriell@gmail.com>
2025-02-24 15:47:21 +0200

    feature(interactive-card-details-form-markup): created semantically correct markup

--- Client/src/features/card-details-form/ui/card-details-form.tsx
+++ Client/src/features/card-details-form/ui/card-details-form.tsx
@@ -1,5 +1,53 @@
-import { type FC } from "react";
+import { type FC, useId } from "react";
 
 export const CardDetailsForm: FC = () => {
-	return null;
+	const cardHolderFullNameInputId = useId();
+	const cardNumberInputId = useId();
+	const cardExpiryMonthInputId = useId();
+	const cardExpiryYearInputId = useId();
+	const cardCvcCodeInputId = useId();
+
+	return (
+		<form>
+			<fieldset>
+				<legend>Card Details</legend>
+				<div>
+					<label htmlFor={cardHolderFullNameInputId}>Cardholder Name</label>
+					<input name="cardHolderFullName" id={cardHolderFullNameInputId} type="text" />
+				</div>
+				<div>
+					<label htmlFor={cardNumberInputId}>Card Number</label>
+					<input name="cardNumber" id={cardNumberInputId} type="text" />
+				</div>
+				<div>
+					<div>
+						<label>Exp. Date (MM/YY)</label>
+						<div>
+							<div>
+								<label htmlFor={cardExpiryMonthInputId}>Expiry Month</label>
+								<input
+									name="cardExpiryMonth"
+									id={cardExpiryMonthInputId}
+									type="number"
+								/>
+							</div>
+							<div>
+								<label htmlFor={cardExpiryYearInputId}>Expiry Year</label>
+								<input
+									name="cardExpiryYear"
+									id={cardExpiryYearInputId}
+									type="number"
+								/>
+							</div>
+						</div>
+					</div>
+					<div>
+						<label htmlFor={cardCvcCodeInputId}>CVC</label>
+						<input name="cardCvcCode" id={cardCvcCodeInputId} type="number" />
+					</div>
+				</div>
+				<button type="submit">Confirm</button>
+			</fieldset>
+		</form>
+	);
 };
