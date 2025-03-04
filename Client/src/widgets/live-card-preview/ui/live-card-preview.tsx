c26cc65e1700179d58ee2841685f7b0fdbe23ba5
Edanriell <Edanriell@gmail.com>
2025-03-04 21:20:31 +0200

    feature(live-card-preview-refactor-to-composite-pattern): implemented pattern

--- Client/src/widgets/live-card-preview/ui/live-card-preview.tsx
+++ Client/src/widgets/live-card-preview/ui/live-card-preview.tsx
@@ -1,15 +1,24 @@
-import { type FC } from "react";
+import { type FC, type ReactNode } from "react";
 
 import { CardBack } from "./card-back";
 import { CardFront } from "./card-front";
 
 import { CardContainer } from "./styles";
 
-export const LiveCardPreview: FC = () => {
-	return (
-		<CardContainer>
-			<CardFront />
-			<CardBack />
-		</CardContainer>
-	);
+type LiveCardPreviewComponents = {
+	CardFront: typeof CardFront;
+	CardBack: typeof CardBack;
 };
+
+type LiveCardPreviewProps = {
+	children: ReactNode;
+};
+
+type LiveCardPreview = FC<LiveCardPreviewProps> & LiveCardPreviewComponents;
+
+export const LiveCardPreview: LiveCardPreview = ({ children }) => {
+	return <CardContainer>{children}</CardContainer>;
+};
+
+LiveCardPreview.CardFront = CardFront;
+LiveCardPreview.CardBack = CardBack;
