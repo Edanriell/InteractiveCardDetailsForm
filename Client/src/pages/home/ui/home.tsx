08f0c5283b3fdf644e553f192b0116bb196c1510
Edanriell <Edanriell@gmail.com>
2025-03-04 20:43:12 +0200

    feature(credit-card-animations): ready to use custom hocs

--- Client/src/pages/home/ui/home.tsx
+++ Client/src/pages/home/ui/home.tsx
@@ -5,7 +5,10 @@ import { CardDetailsForm } from "@features/card-details-form/ui";
 import { LiveCardPreview } from "@widgets/live-card-preview/ui";
 import { RootLayout } from "@widgets/layout/root/ui";
 
+// import { with3D, withCursorFollow } from "@shared/lib/hocs";
 import { Page, PageSection, PageTitle } from "./styles";
+
+// const ThreeDLiveCardPreviewWithCursorFollow = with3D(withCursorFollow(LiveCardPreview));
 
 export const HomePage: FC = () => {
 	return (
