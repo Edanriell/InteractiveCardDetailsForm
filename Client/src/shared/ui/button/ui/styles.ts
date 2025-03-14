a3d2209b5d982883f024c89d8a87325c024792f9
Edanriell <Edanriell@gmail.com>
2025-03-14 12:14:08 +0200

    feature(button): created button component

---
+++ Client/src/shared/ui/button/ui/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+type ButtonProps = {
+	marginTop?: string;
+};
+
+export const Button = styled.button<ButtonProps>`
+	border-radius: 8rem;
+	width: 100%;
+	height: 53rem;
+	background: var(--deep-violet);
+	font-family: var(--font-family);
+	font-weight: 500;
+	font-size: 18rem;
+	color: var(--white);
+	text-align: center;
+	cursor: pointer;
+	margin-top: ${({ marginTop }) => marginTop || "8rem"};
+
+	@media (width >= 1440px) {
+		margin-top: ${({ marginTop }) => marginTop || "14rem"};
+	}
+`;
