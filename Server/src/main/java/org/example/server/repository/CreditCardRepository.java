7ae884dee54e05cc74d3bc968aa7fa1193f4ed59
Edanriell <Edanriell@gmail.com>
2025-04-04 21:22:43 +0300

    feature(repository): created credit card repository

---
+++ Server/src/main/java/org/example/server/repository/CreditCardRepository.java
@@ -0,0 +1,13 @@
+package org.example.server.repository;
+
+import org.example.server.entities.CreditCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CreditCardRepository extends JpaRepository<CreditCard, Long> {
+	Optional<CreditCard> findByCardNumber(String cardNumber);
+}
+
