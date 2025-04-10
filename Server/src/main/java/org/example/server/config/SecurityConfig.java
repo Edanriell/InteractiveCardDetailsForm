afb4493b030fb6afd1b435ad0f6f5b28161ec3a4
Edanriell <Edanriell@gmail.com>
2025-04-10 15:13:58 +0300

    bugfix(db-error): fixed typos and minor db issues

--- Server/src/main/java/org/example/server/config/SecurityConfig.java
+++ Server/src/main/java/org/example/server/config/SecurityConfig.java
@@ -27,7 +27,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
--- Server/src/main/java/org/example/server/repositories/CreditCardRepository.java
+++ Server/src/main/java/org/example/server/repositories/CreditCardRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface CreditCardRepository extends JpaRepository<CreditCard, Long> {
-	Optional<CreditCard> findByCardNumber(String cardNumber);
+	Optional<CreditCard> findByNumber(String number);
 }
+
 
